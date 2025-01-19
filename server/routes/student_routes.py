20399ee861f543d54e6e763a9e24120acc0da7df
NathanLukito <S10244400@connect.np.edu.sg>
2025-01-19 17:30:07 +0800

    feat: Connect to database
    Testing database connection and displaying students

---
+++ server/routes/student_routes.py
@@ -0,0 +1,10 @@
+from flask import Blueprint, jsonify, request
+from controllers.student_controller import get_all_students
+
+student_bp = Blueprint('student_bp', __name__)
+
+@student_bp.route("/", methods=["GET"])
+def list_students():
+    students = get_all_students()
+    return jsonify(students)
+
