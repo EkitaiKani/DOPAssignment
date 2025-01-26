64732c8b374abd6bc4da03203c5ed9e0e5324cd2
NathanLukito <S10244400@connect.np.edu.sg>
2025-01-26 18:55:01 +0800

    Feat: Develop Admin pages
    This feature allows admins to see all student accounts, search for a specific student, modify student accounts, delete student accounts and create new student accounts

--- server/routes/student_routes.py
+++ server/routes/student_routes.py
@@ -1,13 +1,32 @@
-from flask import Blueprint, jsonify, request
-from controllers.student_controller import get_all_students
+from flask import Blueprint, request
+from controllers.student_controller import list_students, search_student_by_id, search_student_by_name, create_student, modify_student, delete_student
 
 student_bp = Blueprint('student_bp', __name__)
 
 @student_bp.route("/", methods=["GET"])
-def list_students():
-    students = get_all_students()
-    return jsonify(students)
+def list_all_students():
+    return list_students()
 
+@student_bp.route("/<student_id>", methods=["GET"])
+def get_student_by_id(student_id):
+    return search_student_by_id(student_id)
 
+@student_bp.route("/search", methods=["GET"])
+def get_student_by_name():
+    identifier = request.args.get('identifier')
+    if len(identifier) == 9 and any(char.isdigit() for char in identifier):
+        return search_student_by_id(identifier)
+    else:
+        return search_student_by_name(identifier)
 
+@student_bp.route("/add_student", methods=["POST"])
+def add_student():
+    return create_student()
 
+@student_bp.route("/<student_id>", methods=["PUT"])
+def update_student(student_id):
+    return modify_student(student_id)
+
+@student_bp.route("/<student_id>", methods=["DELETE"])
+def remove_student(student_id):
+    return delete_student(student_id)
