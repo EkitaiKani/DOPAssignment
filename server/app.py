0edaa3079c20f1ba1a8ad4a4720da275631f725d
NathanLukito <S10244400@connect.np.edu.sg>
2025-01-27 14:54:18 +0800

    fix: Add default port number

--- server/app.py
+++ server/app.py
@@ -62,4 +62,5 @@ def logout():
 
 
 if __name__ == "__main__":
-    app.run(debug=(DevelopmentConfig != ProductionConfig))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=(DevelopmentConfig != ProductionConfig))
